fd546f8d41de4efe5a6b72a7e363a01714371ba8
WojtekM113 <wojtek.mendlik@gmail.com>
2024-11-27 14:48:43 +0100

    commit
    first iteration of player movement using kinematic body

--- Assets/Core/Player/PlayerMovement.cs
+++ Assets/Core/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,7 +10,20 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float maxSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float gravityModifier;
+
+    // radius around sphere where it detects collisions
+    private float _shellRadius = 0.01f;
+    private RaycastHit2D[] _hitBuffer = new RaycastHit2D[16];
+
+    private Vector2 _groundNormal;
+    public float minGroundNormalY = .65f;
+    
+    
     public Vector2 velocity;
+    private Vector2 targetVelocity;
+
+    private float _minMoveDistance = 0.001f;
     
     // Start is called before the first frame update
     void Start()
@@ -19,7 +33,8 @@ public class NewBehaviourScript : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        velocity.x = context.ReadValue<float>();
+        targetVelocity.x = context.ReadValue<float>();
+        targetVelocity.x *= maxSpeed;
     }
 
     public void OnJump(InputAction.CallbackContext context)
@@ -29,19 +44,78 @@ public class NewBehaviourScript : MonoBehaviour
             _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
+
+    private void Update()
+    {
  
+    }
 
     private void FixedUpdate()
     {
-        _rigidbody2D.position += velocity * (maxSpeed * Time.deltaTime);
-        _isGrounded = IsGrounded();
+  
+        velocity += Physics2D.gravity * gravityModifier * Time.deltaTime;
+     
+  
+        _isGrounded = false;
+
+        velocity.x = targetVelocity.x;
+        
+
+        var deltaPosition = velocity * Time.deltaTime;
+
+        var moveAlongGround = new Vector2(_groundNormal.y, -_groundNormal.x);
+
+        var move = moveAlongGround * deltaPosition.x;
+        
+        PerformMovement(move,false);
+       
+        move = Vector2.up * deltaPosition.y;
+
+        PerformMovement(move, true);
     }
 
-    private bool IsGrounded()
+    private void PerformMovement(Vector2 move, bool yMovement)
     {
-        const float groundCheckDistance = 0.1f;
-        var hitCount = _rigidbody2D.Cast(Vector2.down, new RaycastHit2D[1], groundCheckDistance);
+        var distance = move.magnitude;
+        
+        if (distance > _minMoveDistance)
+        {
+            var count = _rigidbody2D.Cast(velocity,_hitBuffer, distance + _shellRadius);
+            for (var i = 0; i < count; i++)
+            {
+                var currentNormal = _hitBuffer[i].normal;
+                if (currentNormal.y > minGroundNormalY)
+                {
+                    _isGrounded = true;
+                    if (yMovement)
+                    {
+                        _groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+                if (_isGrounded)
+                {
+                    var projection = Vector2.Dot(velocity, currentNormal);
+                    if (projection > 0)
+                    {
+                        velocity -= projection * currentNormal;
+                    }
 
-        return hitCount > 0;
+                    velocity.y = 0;
+    
+                }
+                else
+                {
+                    //We are airborne, but hit something, so cancel vertical up and horizontal velocity.
+                    velocity.x *= 0;
+                    velocity.y = Mathf.Min(velocity.y, 0);
+                }
+                var modifiedDistance = _hitBuffer[i].distance - _shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+
+        _rigidbody2D.position += move.normalized * distance;
     }
+    
 }
